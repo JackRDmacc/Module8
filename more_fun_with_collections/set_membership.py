8b51b55e4fb38fde53f898fcdb105cad460e44b2
Reser, Jack C <jcreser@dmacc.edu>
2020-10-19 19:09:51 -0500

    updated function and added docstring

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -1,8 +1,8 @@
 """Jack Reser
-This program
+This program checks if an element is in a set
 10/19/20"""
 
 
-def in_set(set, element):
-    return element in set
+def in_set(my_set, element):
+    return element in my_set
 
