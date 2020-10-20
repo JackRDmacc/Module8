752c45371710354da5a8c0406099fe4516756797
Reser, Jack C <jcreser@dmacc.edu>
2020-10-19 21:46:00 -0500

    updated function. tests pass

--- more_fun_with_collections/date_time_birthday.py
+++ more_fun_with_collections/date_time_birthday.py
@@ -3,7 +3,12 @@ This program calculates a half-birthday
 10/19/20"""
 
 import datetime
+import dateutil
+from dateutil.relativedelta import relativedelta
 
 
-def half_birthday(date):
-    pass
+def half_birthday(my_b_day):
+    my_b_day = my_b_day.date()
+    my_half_b_day = my_b_day + relativedelta(months=6)
+
+    return my_half_b_day
--- test/test_date_time_birthday.py
+++ test/test_date_time_birthday.py
@@ -6,7 +6,7 @@ from more_fun_with_collections import date_time_birthday
 class MyTestCase(unittest.TestCase):
     def test_half_birthday(self):
         birthday = datetime.datetime(2020, 6, 19)
-        self.assertEqual(date_time_birthday.half_birthday(birthday), datetime.datetime(2020, 12, 19))
+        self.assertEqual(date_time_birthday.half_birthday(birthday), datetime.date(2020, 12, 19))
 
 
 if __name__ == '__main__':
