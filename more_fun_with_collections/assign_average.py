e90be9dbeb470ed2c7a33321e9f8184ce05e6585
Reser, Jack C <jcreser@dmacc.edu>
2020-10-19 21:13:13 -0500

    updated function. tests pass

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -1,7 +1,18 @@
 """Jack Reser
-This program
+This program implements pythons version of switch case
 10/19/20"""
 
 
 def switch_average(my_key):
-    pass
+    switch_case_dict = {'A': 'You entered an A!',
+                        'B': 'You entered a B!',
+                        'C': 'You entered a C!',
+                        'D': 'You entered a D!',
+                        'F': 'You entered an F!'}
+
+    result = switch_case_dict.get(my_key, "This is not a valid Grade")
+    return result
+
+
+if __name__ == '__main__':
+    print(switch_average('A'))
--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -4,22 +4,22 @@ from more_fun_with_collections import assign_average
 
 class MyTestCase(unittest.TestCase):
     def test_a(self):
-        self.assertEqual(assign_average.switch_average('A'), 'a')
+        self.assertEqual(assign_average.switch_average('A'), 'You entered an A!')
 
     def test_b(self):
-        self.assertEqual(assign_average.switch_average('B'), 'b')
+        self.assertEqual(assign_average.switch_average('B'), 'You entered a B!')
 
     def test_c(self):
-        self.assertEqual(assign_average.switch_average('C'), 'c')
+        self.assertEqual(assign_average.switch_average('C'), 'You entered a C!')
 
     def test_d(self):
-        self.assertEqual(assign_average.switch_average('D'), 'd')
+        self.assertEqual(assign_average.switch_average('D'), 'You entered a D!')
 
-    def test_e(self):
-        self.assertEqual(assign_average.switch_average('E'), 'e')
+    def test_f(self):
+        self.assertEqual(assign_average.switch_average('F'), 'You entered an F!')
 
     def test_non_key(self):
-        self.assertEqual(assign_average.switch_average('F'), 'not found')
+        self.assertEqual(assign_average.switch_average('Z'), 'This is not a valid Grade')
 
 
 if __name__ == '__main__':
